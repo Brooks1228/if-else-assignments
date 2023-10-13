e3670a53688fef882e36b090adb02c098384b387
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2023-10-13 08:27:29 -0400

    new stuff

---
+++ ex3.vb
@@ -0,0 +1,51 @@
+﻿Module ex3
+    'brooks keller
+    '10/12/23
+    'ex3
+    Sub main()
+        Console.WriteLine("brooks keller" & vbNewLine)
+
+        Console.WriteLine("{0} {1} {2} {3} {4}", "customer".PadRight(20), "magazine".PadRight(20), "total".PadRight(20), "payment".PadRight(20), "payment".PadRight(20))
+        Console.WriteLine("{0} {1} {2} {3} {4}", "name".PadRight(20), "name".PadRight(20), "cost".PadRight(20), "made".PadRight(20), "status".PadRight(20))
+        Console.WriteLine("".PadRight(100, "-"))
+        Dim st1, st2, st3, st4 As String
+        Dim cost1, cost2, cost3, cost4, pay1, pay2, pay3, pay4 As Double
+
+        cost1 = 12.99
+        cost2 = 25.0
+        cost3 = 19.95
+        cost4 = 16.5
+        pay1 = 0.00
+        pay2 = 25.0
+        pay3 = 13.5
+        pay4 = 0
+
+        If cost1 <> pay1 Then
+            st1 = "Payment due"
+        Else
+            st1 = "Payment made"
+        End If
+        If cost2 <> pay2 Then
+            st2 = "Payment due"
+        Else
+            st2 = "Payment made"
+        End If
+        If cost3 <> pay3 Then
+            st3 = "Payment due"
+        Else
+            st3 = "Payment made"
+        End If
+        If cost4 <> pay4 Then
+            st4 = "Payment due"
+        Else
+            st4 = "Payment made"
+        End If
+
+        Console.WriteLine("{0} {1} {2} {3} {4}", "J.R. Sample".PadRight(20), "Redbook".PadRight(20), cost1.ToString("C2").PadRight(20), pay1.ToString("C2").PadRight(20), st1.PadRight(20))
+        Console.WriteLine("{0} {1} {2} {3} {4}", "Lester Hayes".PadRight(20), "Yankee".PadRight(20), cost2.ToString("C2").PadRight(20), pay2.ToString("C2").PadRight(20), st2.PadRight(20))
+        Console.WriteLine("{0} {1} {2} {3} {4}", "Bill White".PadRight(20), "Runner".PadRight(20), cost3.ToString("C2").PadRight(20), pay3.ToString("C2").PadRight(20), st3.PadRight(20))
+        Console.WriteLine("{0} {1} {2} {3} {4}", "Wayne Jones".PadRight(20), "Today".PadRight(20), cost4.ToString("C2").PadRight(20), pay4.ToString("C2").PadRight(20), st4.PadRight(20))
+        Console.WriteLine("".PadRight(100, "-"))
+
+    End Sub
+End Module
---
+++ ex4.vb
@@ -0,0 +1,75 @@
+﻿Module ex4
+    'brooks keller
+    '10/12/23
+    'ex4
+    Sub main()
+        Console.WriteLine("brooks keller" & vbNewLine)
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "student".PadRight(15), "subject".PadRight(15), "grade".PadRight(15), "grade".PadRight(15), "grade".PadRight(15), "grade".PadRight(15), "grade".PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "name".PadRight(15), "".PadRight(15), "1".PadRight(15), "2".PadRight(15), "3".PadRight(15), "avg".PadRight(15), "status".PadRight(15))
+        Console.WriteLine("".PadRight(110, "-"))
+
+        Dim av1, av2, av3, av4, av5, av6 As Double
+        Dim s1, s2, s3, s4, s5, s6 As String
+        av1 = (88 + 89 + 85) / 3
+        av2 = (54 + 59 + 55) / 3
+        av3 = (91 + 99 + 92) / 3
+        av4 = (77 + 75 + 78) / 3
+        av5 = (79 + 79 + 75) / 3
+        av6 = (72 + 73 + 65) / 3
+
+        If av1 > 90 Then
+            s1 = "honors"
+        ElseIf av1 < 70 Then
+            s1 = "failing"
+        Else
+            s1 = ""
+        End If
+
+        If av2 > 90 Then
+            s2 = "honors"
+        ElseIf av2 < 70 Then
+            s2 = "failing"
+        Else
+            s2 = ""
+        End If
+
+        If av3 > 90 Then
+            s3 = "honors"
+        ElseIf av3 < 70 Then
+            s3 = "failing"
+        Else
+            s3 = ""
+        End If
+
+        If av4 > 90 Then
+            s4 = "honors"
+        ElseIf av4 < 70 Then
+            s4 = "failing"
+        Else
+            s4 = ""
+        End If
+
+        If av5 > 90 Then
+            s5 = "honors"
+        ElseIf av5 < 70 Then
+            s5 = "failing"
+        Else
+            s5 = ""
+        End If
+
+        If av6 > 90 Then
+            s6 = "honors"
+        ElseIf av6 < 70 Then
+            s6 = "failing"
+        Else
+            s6 = ""
+        End If
+
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "white".PadRight(15), "chemistry".PadRight(15), "88".PadRight(15), "89".PadRight(15), "85".PadRight(15), av1.ToString("N2").PadRight(15), s1.PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "parker".PadRight(15), "history".PadRight(15), "54".PadRight(15), "59".PadRight(15), "55".PadRight(15), av2.ToString("N2").PadRight(15), s2.PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "jones".PadRight(15), "astronomy".PadRight(15), "91".PadRight(15), "99".PadRight(15), "92".PadRight(15), av3.ToString("N2").PadRight(15), s3.PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "miller".PadRight(15), "biology".PadRight(15), "77".PadRight(15), "75".PadRight(15), "78".PadRight(15), av4.ToString("N2").PadRight(15), s4.PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "harper".PadRight(15), "chemistry".PadRight(15), "79".PadRight(15), "79".PadRight(15), "75".PadRight(15), av5.ToString("N2").PadRight(15), s5.PadRight(15))
+        Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", "hooper".PadRight(15), "algebra".PadRight(15), "72".PadRight(15), "73".PadRight(15), "65".PadRight(15), av6.ToString("N2").PadRight(15), s6.PadRight(15))
+    End Sub
+End Module
